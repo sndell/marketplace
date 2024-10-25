3b9a46a6bc3b3247c5c0d8b508882f5025ca1a7b
sndell <dev.besu@gmail.com>
2024-10-25 20:13:32 +0200

    Revert "fix: change referrer policy"
    This reverts commit 253250c6bdd11ccb793b3dd9be67c50d30a8657b.

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/:path*', // Apply to all routes
-        headers: [
-          {
-            key: 'Referrer-Policy',
-            value: 'no-referrer', // Use a less strict policy, or "origin" or "no-referrer-when-downgrade" as needed
-          },
-        ],
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
